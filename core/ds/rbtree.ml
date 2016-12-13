792cfc3c3954ab834de4507e90b26c920fd080f0
Richard Davison <richard.neil.davison@gmail.com>
2016-12-13 11:43:15 -0500

    Added some more functions to rbtree

--- core/ds/rbtree.ml
+++ core/ds/rbtree.ml
@@ -12,9 +12,55 @@ and 'a node = {
 
 and color = Red | Black
 
+and ins_case =
+  | Case1
+  | Case2
+  | Case3
+  | Case4
+  | Case5
+
+let ( >>= ) m f =
+  match m with
+  | Empty -> Empty
+  | Node n -> f n
+
+let ( >>| ) m f =
+  match m with
+  | Empty -> Empty
+  | Node n -> Node (f n)
+
 let init () = Empty
 
-let caseof t = `Case1
+let parent { parent } = parent
+let left { left } = left
+let right { right } = parent
+
+let sibling t =
+  t.parent >>= fun p ->
+  p.left >>= fun pl ->
+  if pl = t then
+    p.right
+  else
+    p.left
+
+let uncle t =
+  t >>= parent >>= sibling
+
+let grandparent t =
+  t >>= parent >>= parent
+
+let caseof t =
+  match t with
+  | Empty -> Case1
+  | Node { color = Black } -> Case2
+  | Node { color = Red } as node -> raise Not_found
+
+
+let do_case1 t = ()
+let do_case2 t = ()
+let do_case3 t = ()
+let do_case4 t = ()
+let do_case5 t = ()
 
 let insert a tree =
   let rec aux node parent =
@@ -25,18 +71,22 @@ let insert a tree =
         else if a > data then
           aux right node
         else
-          node
+          node, parent
     | Empty ->
-        Node { data   = a;
-               left   = Empty;
-               right  = Empty;
-               parent = parent;
-               color  = Red; }
+        let node =
+          Node { data   = a;
+                 left   = Empty;
+                 right  = Empty;
+                 parent = parent;
+                 color  = Red; }
+        in
+        node, parent
   in
-  let tree = aux tree Empty in
-  match caseof tree with
-  | `Case1 -> ()
-  | `Case2 -> ()
-  | `Case3 -> ()
-  | `Case4 -> ()
-  | `Case5 -> ()
+  let node, parent = aux tree Empty in
+  (* dispatch *)
+  node |> (match caseof parent with
+           | Case1 -> do_case1
+           | Case2 -> do_case2
+           | Case3 -> do_case3
+           | Case4 -> do_case4
+           | Case5 -> do_case5)
